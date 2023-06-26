7a50b67812d54004464614a605a935f4b403fc2c
ParkerPerry <perrypark24@gmail.com>
2023-06-26 16:04:00 -0700

    Finished the assessment

--- src/index.js
+++ src/index.js
@@ -53,3 +53,52 @@ root.render(
     </BackgroundColorWrapper>
   </ThemeContextWrapper>
 );
+
+import detectEthereumProvider from '@metamask/detect-provider';
+
+//import { create as detectEthereumProvider } from '@metamask/detect-provider';
+
+
+// Detect MetaMask provider
+detectEthereumProvider()
+  .then(provider => {
+    if (provider) {
+      // Request access to the user's MetaMask accounts
+      provider.request({ method: 'eth_requestAccounts' })
+        .then(accounts => {
+          const address = accounts[0];
+          const textToSign = "Hello, Chainfuse!";
+
+          // Sign the text with MetaMask
+          provider.request({
+            method: 'personal_sign',
+            params: [textToSign, address],
+          })
+          .then(result => {
+            console.log("Signed message:", result);
+
+            // Get the transaction hash from the signed result
+            const transactionHash = result;
+
+            // Log the transaction hash
+            console.log("Transaction Hash:", transactionHash);
+            // Handle the signed message and transaction hash here
+          })
+          .catch(error => {
+            console.error("Signing error:", error);
+            // Handle signing error here
+          });
+        })
+        .catch(error => {
+          console.error("MetaMask access error:", error);
+          // Handle MetaMask access error here
+        });
+    } else {
+      console.error("MetaMask not detected");
+      // Handle MetaMask not detected error here
+    }
+  })
+  .catch(error => {
+    console.error("Provider detection error:", error);
+    // Handle provider detection error here
+  });
